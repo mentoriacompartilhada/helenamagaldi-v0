41d0b7c43ab9edbe4c2a4e868f2b57e6da9c13be
Helena Magaldi <helenamagaldi@gmail.com>
2021-08-25 15:41:31 -0300

    bugfix/import render module

--- p1/meuapp/views.py
+++ p1/meuapp/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import ClientSerializer
 from .models import Client
 
 def hello_world(request):
-    return HttpResponse("Hello World")
+    return HttpResponse('Hello World')
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all().order_by("nickname")
